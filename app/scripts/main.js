9664d3f883d00059828ec92fb95a7413ac4607ad
Josh Trout <jtrout@gannett.com>
2014-02-07 15:55:35 -0500

    With functionality

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,7 +7,11 @@ $(function () {
 
 	Hammer(targetEl, { drag: false, tap: false, transform: false, hold: false, swipe_velocity: 0.2 }).on('swipe', function (e) {
 		console.log('Someone touched this');
-		console.dir(e);
+		if (e.gesture.direction === Hammer.DIRECTION_LEFT) {
+			$('#hammertime').carousel('next');
+		} else if (e.gesture.direction === Hammer.DIRECTION_RIGHT) {
+			$('#hammertime').carousel('prev');
+		}
 	});
 
 });
