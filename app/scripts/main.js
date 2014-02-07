392ac0fd2184f98174f4583c73cc29af10b888ca
Josh Trout <jtrout@gannett.com>
2014-02-07 15:07:36 -0500

    Step 1 - Adding touch

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,4 +3,11 @@ $(function () {
 
 	$('#hammertime').carousel({ interval: false });
 
+	var targetEl = document.getElementById('hammertime');
+
+	Hammer(targetEl).on('swipe', function (e) {
+		console.log('Someone touched this');
+		console.dir(e);
+	});
+
 });
